e95ffe59e4c60d8fcabef1112df7c0e0840c56ed
Shahar <sshaharse1@gmail.com>
2020-04-06 16:33:43 +0300

    Fixed the tests

--- config/config_test.go
+++ config/config_test.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"reflect"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -12,7 +12,7 @@ func TestGetConfig(t *testing.T) {
 
 	var connection Connection
 	connection.ConnectionType = "tcp"
-	connection.Params = map[string]string{}
+	connection.Params = map[string]interface{}{}
 	connection.ProcessRegex = "."
 	connection.PortRange = "1234"
 	var actions []Action
@@ -20,40 +20,35 @@ func TestGetConfig(t *testing.T) {
 	var action Action
 	action.TriggerRegex = ".+asdf.+"
 	action.Target = "remote"
-	action.TargetParams = map[string]string{"address":"0.0.0.0:1234"}
+	action.TargetParams = map[string]interface {}{"address":"0.0.0.0:1234"}
 	actions = append(actions, action)
 
 	action.TriggerRegex = ".+abc.+"
 	action.Target = "reply"
-	action.TargetParams = map[string]string{"message":"{0-10}asdf"}
+	action.TargetParams = map[string]interface {}{"message":"{0-10}asdf"}
 	actions = append(actions, action)
 
 	connection.Actions = actions
 	connections = append(connections, connection)
 
 	connection.ConnectionType = "ssl"
-	connection.Params = map[string]string{"key-path":"/tmp/key", "cert-path":"/tmp/cert"}
+	connection.Params = map[string]interface {}{"key-path":"/tmp/key", "cert-path":"/tmp/cert"}
 	connection.ProcessRegex = "."
 	connection.PortRange = "100-200"
 
 	var secondActions []Action
 	action.TriggerRegex = "."
 	action.Target = "remote"
-	action.TargetParams = map[string]string{"address":"127.0.0.1:1758"}
+	action.TargetParams = map[string]interface {}{"address":"127.0.0.1:1758"}
 	secondActions = append(secondActions, action)
 
 	connection.Actions = secondActions
 	connections = append(connections, connection)
 
 	configTesting.Connections = connections
-	SetConfigPath("/home/shahar/go/src/fyssl/config/examples/config.json")
-	if !reflect.DeepEqual(configTesting, GetConfig()) {
 
+	SetConfigPath("examples/config.json")
+	if !assert.Equal(t, &configTesting, GetConfig()) {
+		t.Errorf("Config didn't match expected config")
 	}
-
-
-
-
-
-
 }
