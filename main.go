463b5553b12f88d7594bc2c6b563696ade258e4b
Shahar <sshaharse1@gmail.com>
2020-05-26 23:37:04 +0300

    Added help messages and argument processing

--- main.go
+++ main.go
@@ -1,11 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"fyssl/config"
 	"fyssl/connection"
+	"log"
+	"os"
+	"slogger"
 )
 
+func processArgs() {
+	processHelp()
+	processConfig()
+}
+
+func printHelp() {
+	fmt.Println("-h   Help" +
+		"\n-c   ConfigPath, usage: -c {path}")
+}
+
+func processHelp() {
+	if doesArgExist("-h") != -1 {
+		printHelp()
+		log.Fatal("No config file found")
+	}
+}
+
+func processConfig() {
+	if doesArgExist("-c") != -1 {
+		if doesArgExist("-c") + 1 <= len(os.Args[1:]) - 1{
+			configPath := os.Args[1:][doesArgExist("-c") + 1]
+			config.SetConfigPath(configPath)
+			slogger.Info(fmt.Sprintf("Set config path to:%s", configPath))
+		} else {
+			printHelp()
+			log.Fatal("No config file found")
+		}
+	} else {
+		printHelp()
+		log.Fatal("No config file found")
+	}
+}
+
+func doesArgExist(arg string) int {
+	args := os.Args[1:]
+	for index, val := range args {
+		if arg == val {
+			return index
+		}
+	}
+	return -1
+}
+
 func main() {
-	config.SetConfigPath("/home/shahar/go/src/fyssl/config/examples/config.json")
+	processArgs()
 	connection.StartConnections()
 }
+
+
